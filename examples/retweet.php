18fdbfc1cbdb042c50edd2c3044005681ed122b5
Milad Rahimi <info@miladrahimi.com>
2021-01-06 22:57:34 +0330

    add examples

--- examples/retweet.php
+++ examples/retweet.php
@@ -6,13 +6,18 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/keys.php';
 
 $bot = TwitterBot::create(CONSUMER_KEY, CONSUMER_SECRET);
+$bot->setOAuthToken(TOKEN, TOKEN_SECRET);
 $response = $bot->api('GET', 'search/tweets.json', [
     'q' => '@france_soir',
     'count' => 1,
 ]);
 
-if ($response->status() == 200) {
-    print_r($response->content());
+if ($response->status() == 200 && count($response->content()['statuses']) > 0) {
+    $tweet = $response->content()['statuses'][0];
+    $response = $bot->api('POST', "statuses/retweet/{$tweet['id']}.json", [
+        'id' => $tweet['id'],
+    ]);
+    print_r($response);
 } else {
     echo $response; // JSON
 }
